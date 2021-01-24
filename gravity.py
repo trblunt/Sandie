fdb51958497df7434ac7d5c942bab171caf1220c
trblunt <tommybluntjr@gmail.com>
2021-01-23 20:24:53 -0700

    Further optimize gravity.py

--- gravity.py
+++ gravity.py
@@ -22,18 +22,18 @@ def apply_gravity_for_neighborhood(top_left: np.ubyte, top_right: np.ubyte, bott
             top_left, top_right = top_right, top_left
             d_tl, d_tr = d_tr, d_tl
 
-        if f_bl and f_br and (d_tl == -128 or d_tl <= d_bl) and (d_tr == -128 or d_tr <= d_br):
+        if f_bl and f_br and (d_tl <= d_bl) and (d_tr <= d_br):
             bottom_left, bottom_right = bottom_right, bottom_left
             d_bl, d_br = d_tr, d_tl
 
         #  Elements should fall through fluids if they are more dense than the fluid below them.
 
-        if f_bl and d_tl != -128 and d_tl > d_bl:
+        if f_bl and d_tl > d_bl:
             top_left, bottom_left = bottom_left, top_left
             d_tl, d_bl = d_bl, d_tl
             f_tl, f_bl = f_bl, f_tl
 
-        if f_br and d_tr != -128 and d_tr > d_br:
+        if f_br and d_tr > d_br:
             top_right, bottom_right = bottom_right, top_right
             d_tr, d_br = d_br, d_tr
             f_tr, f_br = f_br, f_tr
@@ -44,9 +44,9 @@ def apply_gravity_for_neighborhood(top_left: np.ubyte, top_right: np.ubyte, bott
         #    - The top element of the pile side must be more dense than both elements of the fluid side.
         #  If these conditions are met, swap the top element of the pile side with the bottom element of the fluid side, collapsing the pile.
 
-        if f_tr and f_br and d_tl != -128 and d_tl > d_tr and d_tl > d_br:
+        if f_tr and f_br and d_tl > d_tr and d_tl > d_br:
             top_left, bottom_right = bottom_right, top_left
-        elif f_tl and f_bl and d_tr != -128 and d_tr > d_tl and d_tr > d_bl:
+        elif f_tl and f_bl and d_tr > d_tl and d_tr > d_bl:
             top_right, bottom_left = bottom_left, top_right
 
     return (top_left, top_right, bottom_left, bottom_right)
